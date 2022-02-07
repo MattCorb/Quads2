6acb4fe4973e82d1123b6c50839f5dc421996bd9
skent22 <90283433+skent22@users.noreply.github.com>
2022-02-07 11:37:27 -0700

    DuhModels

---
+++ Models/Task.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quads.Models
+{
+    public class Task
+    {
+        [Key]
+        [Required]
+        public int TaskID { get; set; }
+        [Required]
+        public string TaskName { get; set; }
+        public DateTime DueDate { get; set; }
+        [Required]
+        public bool IsUrgent { get; set; }
+        [Required]
+        public bool IsImportant { get; set; }
+        public bool IsCompleted { get; set; }
+
+        //Build foreign Key Relationship
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+
+    }
+
+}
+
